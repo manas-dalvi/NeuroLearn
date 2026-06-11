1e2af04242f2fdde8b21d2595b0adc1a554f472d
manas-dalvi <manasdalvi.dm@gmail.com>
2026-06-11 20:37:49 +0530

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  allowedDevOrigins: ["192.168.190.1"],
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
