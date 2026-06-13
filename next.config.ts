936d4f877013f43987cccbaa1c71472f38de81f7
manas-dalvi <manasdalvi.dm@gmail.com>
2026-06-13 19:59:15 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+
   reactStrictMode: true,
 
   allowedDevOrigins: ["192.168.190.1"],
 
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
-  },
-  turbopack: {
-    root: __dirname,
   },
 };
 
